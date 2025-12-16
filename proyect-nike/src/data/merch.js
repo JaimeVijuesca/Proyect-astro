9f3b32ff9b5e10352015e220d6656dca4ead6416
Jaime Vijuesca <jaimevijuescafraca@gmail.com>
2025-12-16 18:40:01 +0100

    Shopper cart

---
+++ proyect-nike/src/data/merch.js
@@ -0,0 +1,74 @@
+export const merch = [
+  {
+    id: 1,
+    name: "Nike Air Max 270 – Hombre",
+    price: 159.99,
+    imageUrl: "https://static.nike.com/a/images/t_web_pdp_936_v2/f_auto/awjogtdnqxniqqk0wpgf/AIR+MAX+270.png", 
+    productUrl: "https://www.nike.com/es/t/air-max-270-zapatillas-fTat1UHa",
+    stock: 25,
+    category: "Zapatillas"
+  },
+  {
+    id: 2,
+    name: "Nike Air Max 270 – Mujer",
+    price: 111.99,
+    imageUrl: "https://static.nike.com/a/images/t_web_pdp_936_v2/f_auto/f707161a-2f05-4a8c-ba29-79043d1a6fbe/W+NIKE+AIR+MAX+270.png",
+    productUrl: "https://www.nike.com/es/t/air-max-270-zapatillas-GKQ0N07K",
+    stock: 18,
+    category: "Zapatillas"
+  },
+  {
+    id: 3,
+    name: "Nike Air Max 270 – Multigénero",
+    price: 159.99,
+    imageUrl: "https://static.nike.com/a/images/t_web_pdp_936_v2/f_auto/skwgyqrbfzhu6uyeh0gg/AIR+MAX+270.png",
+    productUrl: "https://www.nike.com/es/w/air-max-270-zapatillas-5ix6dzy7ok",
+    stock: 30,
+    category: "Zapatillas"
+  },
+  {
+    id: 4,
+    name: "Nike Air Force 1 '07 – Hombre",
+    price: 103.99,
+    imageUrl: "https://static.nike.com/a/images/t_web_pdp_936_v2/f_auto/de10ea80-0c03-4612-8358-6d932b51ac7f/NIKE+AIR+FORCE+1+%2707.png",
+    productUrl: "https://www.nike.com/es/t/air-force-1-07-zapatillas-naKHSECK",
+    stock: 20,
+    category: "Zapatillas"
+  },
+  {
+    id: 5,
+    name: "Nike Air Force 1 '07 – Mujer",
+    price: 129.99,
+    imageUrl: "https://static.nike.com/a/images/t_web_pdp_936_v2/f_auto/efe65f6f-03ee-4c45-89e3-34d89ef93fa2/W+AIR+FORCE+1+%2707.png",
+    productUrl: "https://www.nike.com/es/t/air-force-1-07-zapatillas-Mj8KOPQf",
+    stock: 15,
+    category: "Zapatillas"
+  },
+  {
+    id: 6,
+    name: "Nike Air Force 1 '07 Next Nature – Mujer",
+    price: 129.99,
+    imageUrl: "https://static.nike.com/a/images/t_web_pdp_936_v2/f_auto/5c5318c3-dd11-439d-b818-84f04b8a2977/W+AIR+FORCE+1+%2707+NEXT+NATURE.png",
+    productUrl: "https://www.nike.com/es/t/air-force-1-07-next-nature-zapatillas-tClhG3nG",
+    stock: 12,
+    category: "Zapatillas"
+  },
+  {
+    id: 7,
+    name: "Nike Air Force 1 '07 LV8 – Hombre",
+    price: 129.99,
+    imageUrl: "https://static.nike.com/a/images/t_web_pdp_936_v2/f_auto/aa9e375a-d101-437f-9395-9cf4cc6142be/AIR+FORCE+1+%2707+LV8.png",
+    productUrl: "https://www.nike.com/es/t/air-force-1-07-lv8-zapatillas-vO72tFNp",
+    stock: 10,
+    category: "Zapatillas"
+  },
+  {
+    id: 8,
+    name: "Nike Air Force 1 '07 LX – Mujer",
+    price: 119.99,
+    imageUrl: "https://static.nike.com/a/images/t_web_pdp_936_v2/f_auto/15dddb65-fdc8-48cc-9940-b932fc133783/WMNS+AIR+FORCE+1+%2707+LX.png",
+    productUrl: "https://www.nike.com/es/t/air-force-1-07-lx-zapatillas-uhcBnEbX",
+    stock: 14,
+    category: "Zapatillas"
+  }
+];
